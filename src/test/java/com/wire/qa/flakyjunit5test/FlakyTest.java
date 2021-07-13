a6e90dc65e9c97e9487ca05892cb990ee95ea617
Sven Jost <sven@wire.com>
2021-07-13 14:48:47 +0200

    better randomizer

--- src/test/java/com/wire/qa/flakyjunit5test/FlakyTest.java
+++ src/test/java/com/wire/qa/flakyjunit5test/FlakyTest.java
@@ -2,15 +2,13 @@ package com.wire.qa.flakyjunit5test;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FlakyTest {
 
     @Test
     public void flakyTest() {
-        assertThat(new Random().nextBoolean()).isTrue();
+        assertThat(Math.random() < 0.5).isTrue();
     }
 
     @Test
